6270d8b58ac827766a38f2bb0a4939b3f7ee0518
askeip <paparazzi730@gmail.com>
2017-12-02 17:15:15 +0500

    init commit

---
+++ Extrem_MVC_1/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Extrem_MVC_1.Models;
+
+namespace Extrem_MVC_1.Controllers
+{
+    public class StudentController : Controller
+    {
+        static List<StudentModel> Students = new List<StudentModel>
+        {
+            new StudentModel{StudentID = 1, Name = "Evgeny", Surname = "Petrosyan", Course = 4, Group = "FIIT"}
+        };
+
+        public void CreateStudent([FromBody]StudentModel student)
+        {
+            Students.Add(student);
+        }
+
+        public IActionResult GetStudent(int studentID)
+        {
+            var student = Students.FirstOrDefault(x =>
+                    x.StudentID == studentID);
+            if (student != null)
+                return Json(student);
+            return BadRequest();
+        }
+
+        public void UpdateStudent([FromBody]StudentModel student)
+        {
+            Students[Students.FindIndex(x => x.StudentID == student.StudentID)] = student;
+        }
+
+        public void DeleteStudent(int studentID)
+        {
+            Students.RemoveAt(Students.FindIndex(x => x.StudentID == studentID));
+        }
+    }
+}
---
+++ Extrem_MVC_1/Models/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Extrem_MVC_1.Models
+{
+    public class StudentModel
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Course { get; set; }
+        public string Group { get; set; }
+    }
+}
